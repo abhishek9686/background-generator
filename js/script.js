29380366a1d294305910d99083791fd51198c785
abhishek k <abhi281342@gmail.com>
2020-05-06 16:53:31 +0530

    new changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var _= require('lodash');
+var array=[1,2,3,4,5,6,7,8];
+console.log("answer:",_.without(array,3));
+console.log(_);
 var c1=document.getElementById("c1");
 var c2=document.getElementById("c2");
 var body=document.getElementById("b");
